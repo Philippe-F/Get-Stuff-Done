354818d42d9e4a5c928d73c25e51f0d87fae2f3b
Philippe Fonzin <pfonzin@gmail.com>
2020-06-11 12:56:49 -0400

    Create AddTodo Component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <div class="container">
+      <AddTodo />
       <Todos />
     </div>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodos';
 
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos,
+    AddTodo
   }
 }
 </script>
