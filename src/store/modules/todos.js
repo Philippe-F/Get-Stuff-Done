b7cb55a245cf328c10fb1544017b20345426d55e
Philippe Fonzin <pfonzin@gmail.com>
2020-06-11 12:28:05 -0400

    Mutate state with data fetched from the backend

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -11,7 +11,16 @@
 // (a mutation is what actually manipulates the state). When you create an action it takes in an 
 // object with a few items in it but we only need "commit" 
 // ("commit" is what we use to commit a mutation, we don't call a mutions directly
-// we call it with "commit").    
+// we call it with "commit").   
+
+// A mutation is how we are going to add data to our state. So when "fetchTodos"
+// is called in the "created()" functon (this is a lifecylce functon that is 
+// called before the component loads) in the Todos Component. An async HTTP request 
+// is sent to the backend, once the promise is resolved we use commit to call the 
+// "setTodos" mutation, "commit" takes in the mutation type (key) as the first argument
+// the second argument is the mutation payload (if the mutation takes a second argument
+// after state). In our case we pass in the todos (response.data) as the second arg,
+// then the "setTodos" mutation is fired off and sent down to the component. 
 
 import axios from "axios"; 
 
@@ -23,12 +32,14 @@ const getters = {
   allTodos: (state) => state.todos
 };
 const actions = {
-  async fetchTodos() {
+  async fetchTodos({ commit }) {
     const response = await axios.get("https://jsonplaceholder.typicode.com/todos")
-    console.log(response.data);
+    commit("setTodos", response.data);
   }
 };
-const mutations = {};
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos) 
+};
 
 export default {
   state: state,
