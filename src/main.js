65c059ab055081ce987f9760b8f092769c7ee4f8
Philippe Fonzin <pfonzin@gmail.com>
2020-06-11 09:07:16 -0400

    Create store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Store from "./store/index"; 
+// Add the centralized store for all of our state into the Vue instance 
 
 Vue.config.productionTip = false
 
 new Vue({
+  Store, 
   render: h => h(App),
 }).$mount('#app')
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+// This store folder is a centralized store for all of our state (data). 
+
+// This is an entrypoint to Vuex and where we combine all of our modu
+
+import Vuex from "vuex"; 
+import Vue from "vue";
+import Todos from "../modules/Todos"; 
+
+// Load Vue
+Vue.use(Vuex); 
+
+// Create Store 
+export default new Vuex.Store({
+  // This will export all the modules in our app 
+  modules: {
+    Todos
+  }
+})
